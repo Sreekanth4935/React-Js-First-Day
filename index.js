cc7101d1b383d0a4607a0463cd86bfe09ccd5f78
sreekanth <karingulasreekanth024@gmail.com>
2023-02-04 13:14:41 +0000

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+const element = (
+  <div>
+    <h1 className="greeting">Hello sreekanth </h1>
+    <p> Good to see you </p>
+  </div>
+);
+
+//   const fullName = (user) => user.firstName + " " + user.lastName;
+//   const user = { firstName: "karingula", lastName: "Sreekanth" };
+//   const element = <h1 className="greeting">Hello, {fullName(user)}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
